5438a4e51f50b8fe387a0853d10c3bc8ea80dc85
tep yith <tep.yith@gmail.com>
2021-05-16 21:57:59 -0400

    Marker moves when you click on flat card.

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -4,16 +4,7 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = () => <div className="marker"></div>;
 
 class SimpleMap extends Component {
-  // static defaultProps = {
-  //   center: {
-  //     lat: 34.0522,
-  //     lng: -118.2437
-  //   },
-  //   zoom: 11
-  // };
-
   render() {
-    console.log(`${this.props.lat}`);
     const API_KEY = "AIzaSyCghJy9kn0hDhPAj8GIYIyYe0r1JMWw6eQ";
 
     return (
@@ -21,9 +12,10 @@ class SimpleMap extends Component {
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: API_KEY }}
-          defaultCenter={{ lat: this.props.lat, lng: this.props.lng }}
+          center={{ lat: this.props.lat, lng: this.props.lng }}
           defaultZoom={11}
         >
+
           <AnyReactComponent
             lat={this.props.lat}
             lng={this.props.lng}
