a4a1d43413f9460251c178a894f0f70ac71f38a9
tep yith <tep.yith@gmail.com>
2021-05-17 12:09:07 -0400

    Added custom marker.

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,6 +3,11 @@ import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = () => <div className="marker"></div>;
 
+const Marker = () => {
+  const pin = "https://www.shareicon.net/data/512x512/2016/10/18/845321_pin_512x512.png"
+  return <div className="marker"><img src={`${pin}`} alt="" /></div>
+}
+
 class SimpleMap extends Component {
   render() {
     const API_KEY = "AIzaSyCghJy9kn0hDhPAj8GIYIyYe0r1JMWw6eQ";
@@ -15,6 +20,7 @@ class SimpleMap extends Component {
           center={{ lat: this.props.lat, lng: this.props.lng }}
           defaultZoom={11}
         >
+          <Marker lat={this.props.lat} lng={this.props.lng} />
 
           <AnyReactComponent
             lat={this.props.lat}
